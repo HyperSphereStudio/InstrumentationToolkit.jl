49f453cf557eeffec02deb6c4d691c145b0434bd
Johnathan Bizzano <bizzanoj@my.erau.edu>
2024-08-30 10:12:44 -0600

    Create InstallHypersphereLibraries.jl

---
+++ InstallHypersphereLibraries.jl
@@ -0,0 +1,6 @@
+using Pkg
+
+Pkg.add(url="https://github.com/HyperSphereStudio/libmousetrap_jll")
+Pkg.add(url="https://github.com/HyperSphereStudio/libsimplecommunicationencoder_jll")
+Pkg.add(url="https://github.com/HyperSphereStudio/Mousetrap.jl")
+Pkg.add(url="https://github.com/HyperSphereStudio/MousetrapMakie.jl")
